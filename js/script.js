417bb61f5c327aebab4ef6ec565422c4bcc66b00
Katherine Yanan Saung <katysaung@gmail.com>
2023-07-24 14:17:19 -0400

    Day1-Psuedocode

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*Your dom project must meet these requirements:
+1-If you build a you Game must have::
+    a-Must be a two player game.
+    b-A win state(High Score) 
+    c-A lose state.
+    d-A way to keep playing if the game is not over.
+    e-Multiple rounds to play: A round must begin, end, and there must be a way to check if the game should continue or the overall game is won or lost 
+Extra 1: A way to Reset and Play Again.
+Extra 2: CSS to give your game a personal and fun style
+Extra 3: Responsive mobile design
+Extra 4: Audio
+*/
+
