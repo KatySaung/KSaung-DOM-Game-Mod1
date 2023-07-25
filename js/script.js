3047cf2b09ff02585887cdbdb9cf6fc0ebe1f2fb
Katherine Yanan Saung <katysaung@gmail.com>
2023-07-25 08:40:54 -0400

    Day 2-TheGame-DOM CSS error msg in browser

--- js/script.js
+++ js/script.js
@@ -9,5 +9,45 @@ Extra 1: A way to Reset and Play Again.
 Extra 2: CSS to give your game a personal and fun style
 Extra 3: Responsive mobile design
 Extra 4: Audio
+Extra 5: MAKE LEVELS for each continued round
 */
 
+const holes = document.querySelectorAll('.hole')
+const mole = document.querySelectorAll('.mole')
+document.querySelector('#score')
+document.querySelector('#time')
+
+//Global variables
+let result = 
+let time = 
+
+//create random function for the holes and random for mole popping out and disappearing
+function randomHole(){
+    // holes.
+
+    //loop through each of thehole and remove the mole. And create a variable for random placement of these moles with the holes in this function.
+    let randomHole =  holes[Math.floor[(Math.random() * 6)]
+    randomHole.classList.add('mole')
+}
+//create function for Eventlistener in a different function from for mouse click on the squares. Use loop for moving through all the holes and if statement for if hit on mole at the hole add points to the score
+//holes.forEach
+    holes.addEventlistener('clicked') () => {
+        //if statement
+        //score and result
+    }
+
+
+//Create function for timer of mole movement.Set on timer for how fast mole will move to each hole and attach the function to Start button.
+function moveMole(){
+    //create variable for timer and use setInterval for the holes. put an ID to variable so can assign to Start button
+   
+}
+//call the randomMole function here and test on DOM
+moveMole()
+
+//Create function of countdown. use setInterval
+function time(){
+    //clear timer to reset 
+    //create alert to user Game Over with player points
+    //clear alert
+}
