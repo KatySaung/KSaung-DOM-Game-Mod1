6732b601fefb51e551a0fc5ae3fb281581218f10
Katherine Yanan Saung <katysaung@gmail.com>
2023-07-25 17:02:30 -0400

    Day 2 -TheGame-DOM CSS corrected.Begin functions

--- js/script.js
+++ js/script.js
@@ -7,38 +7,46 @@
     e-Multiple rounds to play: A round must begin, end, and there must be a way to check if the game should continue or the overall game is won or lost 
 Extra 1: A way to Reset and Play Again.
 Extra 2: CSS to give your game a personal and fun style
-Extra 3: Responsive mobile design
 Extra 4: Audio
 Extra 5: MAKE LEVELS for each continued round
 */
 
 const holes = document.querySelectorAll('.hole')
 const mole = document.querySelectorAll('.mole')
+const cursor = document.querySelector('.cursor')
 document.querySelector('#score')
 document.querySelector('#time')
 
 //Global variables
-let result = 
-let time = 
+let result;
+let time;
+
+//start game functin
+function startGame(){
+    holes.forEach(hole => hole.addEventListener("click",randomHole))
+
+}
+
+
 
 //create random function for the holes and random for mole popping out and disappearing
 function randomHole(){
-    // holes.
-
-    //loop through each of thehole and remove the mole. And create a variable for random placement of these moles with the holes in this function.
-    let randomHole =  holes[Math.floor[(Math.random() * 6)]
-    randomHole.classList.add('mole')
+    for (let i =0; i<5; i++){
+        let i =  Math.floor(Math.random() * holes.length)
+        let hole = holes[i]
+    }
 }
 //create function for Eventlistener in a different function from for mouse click on the squares. Use loop for moving through all the holes and if statement for if hit on mole at the hole add points to the score
 //holes.forEach
-    holes.addEventlistener('clicked') () => {
+    holes.addEventlistener('clicked', () => {
         //if statement
         //score and result
-    }
+    })
 
 
 //Create function for timer of mole movement.Set on timer for how fast mole will move to each hole and attach the function to Start button.
 function moveMole(){
+   
     //create variable for timer and use setInterval for the holes. put an ID to variable so can assign to Start button
    
 }
