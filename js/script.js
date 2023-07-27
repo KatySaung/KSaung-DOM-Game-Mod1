ce3b764f4cda39fd1045919a20a8cfd4115fa56a
Katherine Yanan Saung <katysaung@gmail.com>
2023-07-27 17:07:48 -0400

    completed all buttons functionality

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const timeCounter = document.getElementById("time")
 let time = 10;
 let score = 0;
 let molePosition = 0;
-
+let gameStart = false;
 
 //WORKING-create function to move and remove mole randomly in holes and assign the current mole position to the current hole.
 function moveMole(){
@@ -29,8 +29,7 @@ function speedMoveMole(){
     time = 10;
     moleTimer = setInterval(moveMole,375);
     gameTimer = setInterval(timer,1000);
-
-}
+} 
 
 
 //function to click on the mole at current mole position and score points. NEED TO FIND WAY TO STOP CHEATING WITH MULTIPLE CLICKS.
@@ -40,8 +39,8 @@ holes.forEach((hole) => {
             score++;
             scoreCounter.innerText = score;
             hole.classList.remove("mole");
-        }
-    });
+ } })
+    
 });
 
 //create Game Timer and stop timer at zero and end both mole and timer.//NOT WORKING-STOP TIME SHOWS -1 after CLEAR ALERT GAME OVER.
@@ -53,14 +52,20 @@ function timer(){
         alert("GAME OVER");
         clearInterval(gameTimer);
         clearInterval(moleTimer);    
-    }   
+    }  
 }
 
 //Click Start Button to Start Game
 function startTime(){
-    speedMoveMole();
+    
+    if (!gameStart){
+        speedMoveMole();
+        gameStart = true;
+    }
 }
 startBtn.addEventListener("click", startTime);
+// startBtn.removeEventListener("click", startTime);
+
 
 //Click End Button to Stop Game
 function endTime(){
@@ -68,6 +73,11 @@ function endTime(){
     clearInterval(moleTimer); 
 }
 endBtn.addEventListener("click", endTime);
+// endBtn.removeEventListener("click", endTime);
 
 //Click Reset Button to refresh page
+function reStartTime(){
+    window.location.reload();
 
+}
+reStartBtn.addEventListener("click",reStartTime);
